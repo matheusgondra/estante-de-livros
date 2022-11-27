20f23b3823a3fc3a410df684cbf6113c593c0350
matheusgondra <matheusgondra951@gmail.com>
2022-11-26 22:22:15 -0300

    :sparkles: feat: criando métodos para busca

--- server/src/controllers/BookController.js
+++ server/src/controllers/BookController.js
@@ -1,5 +1,24 @@
-class BookController {
+const models = require("../models");
 
+class BookController {
+	static async getAllBooks(req, res) {
+		try {
+			const books = await models.Book.findAll();
+			return res.status(200).json(books);
+		} catch (error) {
+			return res.status(500).json({ error: error.message });
+		}
+	}
+
+	static async getBook(req, res) {
+		const { id } = req.params;
+		try {
+			const book = await models.Book.findOne({ where: { id } });
+			return res.status(200).json(book);
+		} catch (error) {
+			return res.status(500).json({ error: error.message });
+		}
+	}
 }
 
 module.exports = BookController;
